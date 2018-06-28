5650cb49ecc0236a9126385e932dfffcec8253bd
Daniel Pickert <dpickert1@gmail.com>
2018-06-28 09:57:03 -0400

    first commit

---
+++ main.js
@@ -0,0 +1,25 @@
+// Stack
+
+let stack = [];
+
+stack.push(123)
+stack.push(456)
+stack.push(789)
+
+console.log(stack.pop())
+console.log(stack.pop())
+
+
+// Queue
+
+let queue = [];
+
+queue.push(123)
+queue.push(456)
+queue.push(789)
+
+console.log(queue.shift())
+console.log(queue.shift())
+
+
+
